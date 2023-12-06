737f82c7dd98c337011d736c4014a7d6a320f30d
Jason Li <jasonli99193@gmail.com>
2023-12-06 15:33:48 -0600

    setup

---
+++ src/components/auth/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+import "./Login.css"
+
+export const Login = () => {
+    const [email, set] = useState("hpassfield7@netvibes.com")
+    const navigate = useNavigate()
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        return fetch(`http://localhost:8088/users?email=${email}`)
+            .then(res => res.json())
+            .then(foundUsers => {
+                if (foundUsers.length === 1) {
+                    const user = foundUsers[0]
+                    localStorage.setItem("honey_user", JSON.stringify({
+                        id: user.id,
+                        staff: user.isStaff
+                    }))
+
+                    navigate("/")
+                }
+                else {
+                    window.alert("Invalid login")
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h1>Jason's Goofy Den of Turbulence</h1>
+                    <h2>Enter at your own risk!</h2>
+                    <fieldset>
+                        <button type="submit">
+                            Traverse the Lands of Lordran, without the Lords...
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
+        </main>
+    )
+}
+
